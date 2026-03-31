4ab2fd5375212a4c9c0e7e002f876de58ae690e5
Kevin Davila <keving12machala@gmail.com>
2026-03-31 03:02:32 -0500

    Fix: agregar bar-d-paul.onrender.com a ALLOWED_HOSTS

--- core/settings.py
+++ core/settings.py
@@ -13,8 +13,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-7b)1)muek+03@kj_atf9$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
-# Application definition
+ALLOWED_HOSTS = ['bar-d-paul.onrender.com', '.onrender.com', 'localhost', '127.0.0.1']# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
